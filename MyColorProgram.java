4d84602a9397fca714ecb3c09a70106553532ad6
joellof <joel@lofving.se>
2016-11-10 19:29:34 +0100

    Update MyColorProgram.java

--- MyColorProgram.java
+++ MyColorProgram.java
@@ -37,8 +37,12 @@ public class MyColorProgram {
       int[][][] newSamples = new int[samples.length][samples[0].length][3];
       for (int row = 0; row < samples.length; row = row + 1)
          for (int col = 0; col < samples[row].length; col = col + 1) {
-            double L = 0.299*samples[row][col][0]+0.587*samples[row][col][1]+0.114*samples[row][col][2];
+            
+            double L = 0.299 * samples[row][col][0] + 0.587 * 
+               samples[row][col][1]+ 0.114 * samples[row][col][2];
+            
             int LL = (int)Math.round(L);
+            
             for (int c = 0; c < samples[row][col].length; c = c + 1) {
                newSamples[row][col][c] = LL;
             }
@@ -50,7 +54,10 @@ public class MyColorProgram {
       int[][][] newSamples = new int[samples.length][samples[0].length][3];
       for (int row = 0; row < samples.length; row = row + 1)
          for (int col = 0; col < samples[row].length; col = col + 1) {
-            double L = 0.299 * samples[row][col][0] + 0.587 * samples[row][col][1] + 0.114 * samples[row][col][2];
+            
+            double L = 0.299 * samples[row][col][0] + 0.587 * 
+               samples[row][col][1] + 0.114 * samples[row][col][2];
+            
             for (int c = 0; c < samples[row][col].length; c = c + 1) {
                if (L < 128)
                   newSamples[row][col][c] = 0;
@@ -66,7 +73,8 @@ public class MyColorProgram {
       for (int row = 0; row < samples.length; row = row + 1)
          for (int col = 0; col < samples[row].length; col = col + 1)
             for (int c = 0; c < samples[row][col].length; c = c + 1) {
-               if (row > 0 && col > 0 && row < (samples.length - 1) && col < (samples[0].length - 1)) {
+               if (row > 0 && col > 0 && row < (samples.length - 1) && 
+                   col < (samples[0].length - 1)) {
                   newSamples[row][col][c] = ((5 * samples[row][col][c])
                           - (samples[row - 1][col][c])
                           - (samples[row ][col - 1][c])
@@ -86,7 +94,8 @@ public class MyColorProgram {
       for (int row = 0; row < samples.length; row = row + 1)
          for (int col = 0; col < samples[row].length; col = col + 1)
             for (int c = 0; c < samples[row][col].length; c = c + 1) {
-               if (row > 0 && col > 0 && row < (samples.length - 1) && col < (samples[0].length - 1)) {
+               if (row > 0 && col > 0 && row < (samples.length - 1) &&
+                   col < (samples[0].length - 1)) {
                   newSamples[row][col][c] = ((9 * samples[row][col][c])
                           - (samples[row - 1][col][c]) - (samples[row ][col - 1][c])
                           - (samples[row ][col + 1][c])
@@ -111,7 +120,8 @@ public class MyColorProgram {
       for (int row = 0; row < samples.length; row = row + 1)
          for (int col = 0; col < samples[row].length; col = col + 1)
             for (int c = 0; c < samples[row][col].length; c = c + 1) {
-               if (row > 0 && col > 0 && row < (samples.length - 1) && col < (samples[0].length - 1)) {
+               if (row > 0 && col > 0 && row < (samples.length - 1) &&
+                   col < (samples[0].length - 1)) {
                   newSamplesX = ((-1)*(samples[row-1][col-1][c])
                           - 2*(samples[row - 1][col][c])
                           - (samples[row -1][col +1][c])
@@ -126,7 +136,8 @@ public class MyColorProgram {
                           + 2*(samples[row ][col + 1][c])
                           + (samples[row + 1][col + 1][c]));
 
-                  newSamples[row][col][c] = (int)Math.sqrt(Math.pow(newSamplesX, 2) + Math.pow(newSamplesY,2));
+                  newSamples[row][col][c] = (int)Math.sqrt(Math.pow(newSamplesX, 2) + 
+                                                           Math.pow(newSamplesY,2));
 
                   if (newSamples[row][col][c] < 0)
                      newSamples[row][col][c] = 0;
